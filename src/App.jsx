26a034911631a11bbc1525b6506ae351f82848d0
dev-nirob1 <dev-nirob1@gmail.com>
2024-01-21 16:37:38 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-center text-2xl text-red-500">hello world</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/shared/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+    return (
+        <div className="container mx-auto">
+            <nav className="py-3 shadow flex items-center justify-between">
+                <h2 className="text-3xl font-semibold italic">EchoLens</h2>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
